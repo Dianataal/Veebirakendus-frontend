d4af6f45a0e83b3d94213e6f9a060718b62ef968
Diana <taaldiana@gmail.com>
2023-03-26 21:10:38 +0300

    elering

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const priceRef = useRef();
   const isActiveRef = useRef();
   const [pakiautomaadid, setPakiautomaadid] = useState([]);
+  const [prices, setPrices] = useState([]);
+    const [chosenCountry, setChosenCountry] = useState("ee");
 
 
   useEffect(() => {
@@ -24,9 +26,18 @@ function App() {
 
   useEffect(() => {
     fetch("http://localhost:3000/parcelmachines")
-        .then(res => res.json())
-        .then(json => setPakiautomaadid(json));
-}, []);
+      .then(res => res.json())
+      .then(json => setPakiautomaadid(json));
+  }, []);
+  
+  useEffect(() => {
+    fetch("http://localhost:3000/nord-pool-price/" + chosenCountry)
+      .then(res => res.json())
+      .then(json => {
+        setPrices(json);
+      });
+  }, [chosenCountry]);
+
   ////////////////////////
   function lisa() {
     const uusToode = {
@@ -67,7 +78,30 @@ function App() {
           <button onClick={() => kustuta(index)}>x</button>
         </div>)}
       <button onClick={() => dollariteks()}>Muuda dollariteks</button>
+
+      <div>
+            <button onClick={() => setChosenCountry("fi")}>Soome</button>
+            <button onClick={() => setChosenCountry("ee")}>Eesti</button>
+            <button onClick={() => setChosenCountry("lv")}>Läti</button>
+            <button onClick={() => setChosenCountry("lt")}>Leedu</button>
+            <table style={{marginLeft: "100px"}}>
+                <thead>
+                <th style={{border: "1px solid #ddd", padding: "12px", backgroundColor: "#04AA6D"}}>Ajatempel</th>
+                <th style={{border: "1px solid #ddd", padding: "12px", backgroundColor: "#04AA6D"}}>Hind</th>
+                </thead>
+                <tbody>
+                <div style={{position: "absolute", left: "30px"}}>{chosenCountry}</div>
+                {prices.map(data =>
+                    <tr key={data.timestamp}>
+                        <td style={{border: "1px solid #ddd", padding: "8px"}}>{new Date(data.timestamp * 1000).toISOString()}</td>
+                        <td style={{border: "1px solid #ddd", padding: "8px"}}>{data.price}</td>
+                    </tr>)}
+                </tbody>
+            </table>
     </div>
+    </div>
+
+    
   );
 }
 
